72d0014610087d94cec78885751f562d0357e49a
DELL <snehal18122004@gmail.com>
2023-02-26 10:01:02 +0530

    Initial commit

---
+++ lib/homePage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class home extends StatefulWidget {
+  const home({Key? key}) : super(key: key);
+
+  @override
+  State<home> createState() => _homeState();
+}
+
+class _homeState extends State<home> {
+  @override
+  Widget build(BuildContext context) {
+    return const SafeArea(child: Scaffold(
+
+    ));
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:dies_app/homePage.dart';
+import 'package:flutter/material.dart';
+void main()
+{
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: home(),
+    )
+  );
+}
